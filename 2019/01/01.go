6d3c415a6df5d80119f1195047d32ec7b29179b6
Barton Rhodes <barton.t.rhodes@gmail.com>
2019-12-01 23:00:12 -0700

    Adds calculation for  total fuel requirement

--- 2019/01/01.go
+++ 2019/01/01.go
@@ -1,20 +1,37 @@
 package advent
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"os"
+	"strconv"
 )
 
 // CalculateFuel computes fuel required per module
-func CalculateFuel(mass int) int {
+func CalculateFuel(mass int64) int64 {
 	return mass/3 - 2
 }
 
-func main() {
-	data, err := ioutil.ReadFile("input.txt")
+//SumFuelRequirement reads in module weights
+func SumFuelRequirement(filename string) int64 {
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Println("File reading error", err)
-		return
+		fmt.Println("Failed opening file: ", err)
 	}
-	fmt.Println("I've read:", string(data))
+
+	scanner := bufio.NewScanner(file)
+	var total int64
+	for scanner.Scan() {
+		weight, err := strconv.ParseInt(scanner.Text(), 0, 64)
+		if err != nil {
+			fmt.Println("Error parsing weight", err)
+		}
+		total += CalculateFuel(weight)
+	}
+
+	return total
+}
+
+func main() {
+	fmt.Println(SumFuelRequirement("input.txt"))
 }
