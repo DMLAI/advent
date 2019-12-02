31ba5abd0e770963658a145bf321778ed8767d86
Barton Rhodes <barton.t.rhodes@gmail.com>
2019-12-01 23:18:46 -0700

    Modifies types to int and makes tests pass

--- 2019/01/01.go
+++ 2019/01/01.go
@@ -8,25 +8,25 @@ import (
 )
 
 // CalculateFuel computes fuel required per module
-func CalculateFuel(mass int64) int64 {
+func CalculateFuel(mass int) int {
 	return mass/3 - 2
 }
 
 //SumFuelRequirement reads in module weights
-func SumFuelRequirement(filename string) int64 {
+func SumFuelRequirement(filename string) int {
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("Failed opening file: ", err)
 	}
 
 	scanner := bufio.NewScanner(file)
-	var total int64
+	var total int
 	for scanner.Scan() {
 		weight, err := strconv.ParseInt(scanner.Text(), 0, 64)
 		if err != nil {
 			fmt.Println("Error parsing weight", err)
 		}
-		total += CalculateFuel(weight)
+		total += CalculateFuel(int(weight))
 	}
 
 	return total
